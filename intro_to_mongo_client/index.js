aeebf0d7bfcc48efe5296b1f103531998325431c
tmarr1426 <tmarr1426@gmail.com>
2024-02-01 19:09:48 -0600

    Made full CRUD server

--- intro_to_mongo_client/index.js
+++ intro_to_mongo_client/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 // Importing mongo client from library
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 
 const PORT = process.env.PORT;
 
@@ -59,13 +59,72 @@ app.get("/userdata", async (req, res) => {
     // });
 
     for await (let userObj of userList) {
-        results.push(userObj)
-    };
+      results.push(userObj);
+    }
 
     res.status(200).json({
       Results: results,
     });
   } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+});
+
+app.get("/userdata/:id", async (req, res) => {
+  try {
+    let userColl = await dbConnect();
+    let results = [];
+
+    let userList = userColl.find({ _id: new ObjectId(req.params.id) });
+
+    for await (item of userList) {
+      results.push(item);
+    }
+
+    res.status(200).json({
+      Found: results,
+    });
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+});
+
+app.delete("/userdata/delete/:id", async (req, res) => {
+  try {
+    let userColl = await dbConnect();
+
+    userColl.deleteOne({ _id: new ObjectId(req.params.id) });
+
+    res.status(200).json({
+      Delete: "Success",
+    });
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+});
+
+app.put("/userdata/update/:id", async (req, res) => {
+  try {
+    let userColl = await dbConnect();
+
+    userColl.updateOne(
+      { _id: new ObjectId(req.params.id) },
+      {
+        $set: {
+          _id: new ObjectId(req.params.id),
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+        },
+      }
+    );
+    res.status(200).json({
+        Update: "Success",
+      });
+  } catch (err) {
+    console.log(err);
     res.send(err);
   }
 });
